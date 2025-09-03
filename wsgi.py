11cc30a267764c056c233220897291113efe24da
aglukhovskiy <aleksey.glukhovskiy@gmail.com>
2025-09-03 15:02:24 +0300

    deploy chngs _

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,9 @@
 from api_v2 import app
+from waitress import serve
+import os
+
+port = int(os.environ.get('PORT', 8080))
+print(f"Starting server on port {port}")
 
 if __name__ == "__main__":
-    app.run()
+    serve(app, host='0.0.0.0', port=port)
