d7fcb9156896aff130f9b8b99c47f53d2b1b26ef
aglukhovskiy <aleksey.glukhovskiy@gmail.com>
2025-09-03 13:22:41 +0300

    deploy chngs

--- api_v2.py
+++ api_v2.py
@@ -209,4 +209,5 @@ def bot_move():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
+    print(f"Starting server on port {port}")
     app.run(host='0.0.0.0', port=port, debug=False)
